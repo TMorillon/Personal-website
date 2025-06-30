75659a5e5f91ae3b7035eddc8d0ec410aaa9cd22
TMorillon <141192287+TMorillon@users.noreply.github.com>
2025-06-29 21:43:23 -0400

    Create researchReveal.js

---
+++ script/researchReveal.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+  ScrollReveal().reveal('#research-full', {
+    distance: '60px',
+    origin: 'bottom',
+    duration: 1000,
+    delay: 200,
+    reset: false,
+    easing: 'ease-in-out',
+  });
+
+  // Add more reveals if needed for individual elements
+});
