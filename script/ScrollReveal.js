beacd05ec94c0cba404fca1cafecf9e504ba756e
TMorillon <141192287+TMorillon@users.noreply.github.com>
2025-06-29 22:21:34 -0400

    Update ScrollReveal.js

--- script/ScrollReveal.js
+++ script/ScrollReveal.js
@@ -1,7 +1,18 @@
-ScrollReveal().reveal('section', {
-  distance: '40px',
-  duration: 800,
-  easing: 'ease-in-out',
-  origin: 'bottom',
-  interval: 200
+document.addEventListener('DOMContentLoaded', () => {
+  ScrollReveal().reveal('section', {
+    distance: '40px',
+    duration: 800,
+    easing: 'ease-in-out',
+    origin: 'bottom',
+    interval: 200
+  });
+
+  ScrollReveal().reveal('#research-full', {
+    distance: '60px',
+    origin: 'bottom',
+    duration: 1000,
+    delay: 200,
+    reset: false,
+    easing: 'ease-in-out',
+  });
 });
